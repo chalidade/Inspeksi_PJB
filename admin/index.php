1747cd59c592017964d92095e398bbbf1ecba388
chalidade <chalidade@gmail.com>
2019-06-25 21:00:45 +0700

    Change permission

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,9 @@
 			<?php include "header.php"; ?>
 
+			<?php
+			$jabatan = $_SESSION['jabatan'];
+			if ($jabatan == "Admin") {
+			?>
 			<!-- Start service Area -->
 			<section class="service-area section-gap" id="facilities" style="padding-top:40px;">
 				<div class="container">
@@ -97,5 +101,41 @@
 				</div>
 			</section>
 			<!-- End service Area -->
-
+		<?php } else { ?>
+			<!-- Start service Area -->
+			<section class="service-area section-gap" id="facilities" style="padding-top:40px;">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="col-md-8 pb-10 header-text">
+							<h3>USER ACCOUNT</h3>
+							<p> What will you do ?	</p>
+						</div>
+					</div>
+					<div class="row" style="margin-top:50px; padding:10px">
+						<div class="col-6">
+						  <div class="content">
+						    <a href="menu-laporan.php">
+						  		 <img class="content-image img-fluid d-block mx-auto" style="width:50%;" src="img/schedule.png" alt="">
+						      <div class="content-details fadeIn-bottom">
+						        <!-- <h3 class="content-title"  style="font-size:12px;">Logout </h3> -->
+						      </div>
+						    </a>
+						  </div>
+							<h3 class="content-title" style="font-size:12px;color:#000; text-align:center;margin-top:10px;">Laporan</h3>
+						</div>
+						<div class="col-6">
+							<div class="content">
+								<a href="../index.php">
+									 <img class="content-image img-fluid d-block mx-auto" style="width:50%;" src="img/wall-clock.png" alt="">
+									<div class="content-details fadeIn-bottom">
+										<!-- <h3 class="content-title"  style="font-size:12px;">Logout </h3> -->
+									</div>
+								</a>
+							</div>
+							<h3 class="content-title" style="font-size:12px;color:#000; text-align:center;margin-top:10px;">Logout</h3>
+						</div>
+					</div>
+			</section>
+			<!-- End service Area -->
+		<?php } ?>
 		<?php include "footer.php"; ?>
